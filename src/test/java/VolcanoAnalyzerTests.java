35c07d7100abe4aa165887906d6dce943a3fdbeb
Jeffrey Klier <jeffrey.klier@galvanize.com>
2019-03-14 13:44:41 -0700

    updated method and test names to match js exercise

--- src/test/java/VolcanoAnalyzerTests.java
+++ src/test/java/VolcanoAnalyzerTests.java
@@ -45,12 +45,12 @@ public class VolcanoAnalyzerTests {
     }
 
     @Test
-    public void EruptedInEighties() {
+    public void eruptedInEighties() {
         //Setup
         Integer expected = 40;
 
         //Execute
-        Integer actual = va.EruptedInEighties().size();
+        Integer actual = va.eruptedInEighties().size();
 
         //Assert
         Assert.assertEquals("should have 40 volcanoes", expected, actual);
@@ -59,12 +59,12 @@ public class VolcanoAnalyzerTests {
     }
 
     @Test
-    public void highVEIVolcanoes() {
+    public void highVEI() {
         //Setup
         String[] expected = { "Macauley Island", "Kikai", "Masaya", "Pago", "Taal", "Pinatubo", "Long Island", "Black Peak", "St. Helens", "Veniaminof", "Aniakchak", "Santorini", "Taupo", "Pago", "Pinatubo", "Raoul Island", "Okmok", "Apoyeque", "Ambrym", "Bona-Churchill", "Taupo", "Ksudach", "Ilopango", "Rabaul", "Pago", "Bona-Churchill", "Dakataua", "Ceboruco", "Changbaishan", "Quilotoa", "Kuwae", "Bardarbunga", "Huaynaputina", "Long Island", "Tambora", "Krakatau", "Santa Maria", "Novarupta", "Pinatubo" };
 
         //Execute
-        String[] actual = va.highVEIVolcanoes();
+        String[] actual = va.highVEI();
 
         //Assert
         Assert.assertEquals("should have 39 volcanoes", expected.length, actual.length);
@@ -74,7 +74,7 @@ public class VolcanoAnalyzerTests {
     }
 
     @Test
-    public void deadliestVolcano() {
+    public void mostDeadly() {
         //Setup
         Volcano expected = new Volcano();
         expected.setYear(450);
@@ -92,7 +92,7 @@ public class VolcanoAnalyzerTests {
         expected.setDEATHS("30000");
 
         //Execute
-        Volcano actual = va.deadliestVolcano();
+        Volcano actual = va.mostDeadly();
 
         //Assert
         Assert.assertEquals("should have correct year", expected.getYear(), actual.getYear());
@@ -112,12 +112,12 @@ public class VolcanoAnalyzerTests {
     }
 
     @Test
-    public void tsunamiPercentage() {
+    public void causedTsunami() {
         //Setup
         double expected = 17;
 
         //Execute
-        double actual = va.tsunamiPercentage();
+        double actual = va.causedTsunami();
 
         //Assert
         Assert.assertEquals("should be about 17.0 percent", expected, actual, .1);
@@ -126,12 +126,12 @@ public class VolcanoAnalyzerTests {
     }
 
     @Test
-    public void mostCommonVolcanoType() {
+    public void mostCommonType() {
         //Setup
         String expected = "Stratovolcano";
 
         //Execute
-        String actual = va.mostCommonVolcanoType();
+        String actual = va.mostCommonType();
 
         //Assert
         Assert.assertEquals("should be Stratovolcano", expected, actual);
@@ -159,12 +159,12 @@ public class VolcanoAnalyzerTests {
     }
 
     @Test
-    public void avgEruptionElevation() {
+    public void averageElevation() {
         //Setup
         double expected = 1993.71;
 
         //Execute
-        double actual = va.avgEruptionElevation();
+        double actual = va.averageElevation();
 
         //Assert
         Assert.assertEquals("should be about 1993.71", expected, actual, .1);
@@ -173,11 +173,11 @@ public class VolcanoAnalyzerTests {
     }
 
     @Test
-    public void allVolcanoTypes() {
+    public void volcanoTypes() {
         //Setup
         String[] expected = { "Caldera", "Stratovolcano", "Complex volcano", "Shield volcano", "Pyroclastic shield", "Volcanic field", "Lava dome", "Subglacial volcano", "Crater rows", "Tuff cone", "Fissure vent", "Pyroclastic cone", "Submarine volcano", "Lava cone", "Pumice cone", "Mud volcano", "Cinder cone", "Compound volcano", "Maar"};
         //Execute
-        String[] actual = va.allVolcanoTypes();
+        String[] actual = va.volcanoTypes();
 
         //Assert
         Assert.assertEquals("should have 19 volcano types", expected.length, actual.length);
@@ -187,12 +187,12 @@ public class VolcanoAnalyzerTests {
     }
 
     @Test
-    public void northernHemiEruptionsPercentage() {
+    public void percentNorth() {
         //Setup
         double expected = 64.55;
 
         //Execute
-        double actual = va.northernHemiEruptionsPercentage();
+        double actual = va.percentNorth();
 
         //Assert
         Assert.assertEquals("should be about 64.55 percent", expected, actual, .1);
@@ -201,11 +201,11 @@ public class VolcanoAnalyzerTests {
     }
 
     @Test
-    public void verySpecificVolcanoes() {
+    public void manyFilters() {
         //Setup
         String[] expected = { "Galunggung", "Hudson, Cerro" };
         //Execute
-        String[] actual = va.verySpecificVolcanoes();
+        String[] actual = va.manyFilters();
 
         //Assert
         Assert.assertEquals("should have 3 volcanoes", expected.length, actual.length);
@@ -215,12 +215,12 @@ public class VolcanoAnalyzerTests {
     }
 
     @Test
-    public void volcanoesAboveElevation() {
+    public void elevatedVolcanoes() {
         //Setup
         String[] expected = { "Tungurahua", "Cotopaxi", "Bona-Churchill", "Bona-Churchill", "Ruiz", "Tungurahua", "Cotopaxi", "Cotopaxi", "Cotopaxi", "Cotopaxi", "Sabancaya", "Ararat", "Ruiz", "Cotopaxi", "Tungurahua", "Tungurahua", "Popocatepetl", "Sangay", "Ruiz", "Sabancaya", "Huila", "Popocatepetl", "Tungurahua", "Tungurahua", "Tungurahua", "Huila", "Tungurahua", "Huila", "Tungurahua", "Tungurahua", "Tungurahua", "Ubinas" };
 
         //Execute
-        String[] actual = va.volcanoesAboveElevation(4990);
+        String[] actual = va.elevatedVolcanoes(4990);
 
         //Assert
         Assert.assertEquals("should have 32 volcanoes above 4990", expected.length, actual.length);
@@ -230,12 +230,12 @@ public class VolcanoAnalyzerTests {
     }
 
     @Test
-    public void topTenDeadliestEruptionAgents() {
+    public void topAgentsOfDeath() {
         //Setup
         String[] expected = { "P", "M", "W", "A", "I" };
 
         //Execute
-        String[] actual = va.topTenDeadliestEruptionAgents();
+        String[] actual = va.topAgentsOfDeath();
 
         //Assert
         Assert.assertEquals("should have 5 Agents of Death", expected.length, actual.length);
